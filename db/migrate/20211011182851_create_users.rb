35992de7ca896e410098fc19c9515e3ae75c41f4
lmayancela <lmayancela08@gmail.com>
2021-10-11 15:07:46 -0400

    Fixed migrations and associations for User and House tables

--- db/migrate/20211011182851_create_users.rb
+++ db/migrate/20211011182851_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.name :string
-      t.email :string
-      t.password :string
-      t.house :references
+      t.string :name
+      t.string :email
+      t.string :password
+      t.integer :house_id
 
       t.timestamps
     end
