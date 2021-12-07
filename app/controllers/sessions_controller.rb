24f503d44a46705b1516d011dc733b5e65f66692
lmayancela <lmayancela08@gmail.com>
2021-12-07 06:40:48 -0500

    Main and Bill page display updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    if user.authenticate(params[:session][:password])
+      puts("--------------------------------------------")
+      puts("logging in...")
       reset_session
       log_in user
       redirect_to user
@@ -16,6 +18,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    puts("--------------------------------------------")
+    puts("logging out...")
     log_out
     redirect_to root_url
   end
